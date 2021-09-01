a6b9fb9c9d702f0366b3151fa6235037237fab1c
Divya Thangavelsamy <divthang@WKMZTE892DD6.global.publicisgroupe.net>
2021-09-01 14:41:33 +0100

    UI-colors

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.red[900],
         title: Center(child: Text('My first app')),
       ),
-      body: Center(child: Text('Hello World')),
+      body: Center(child: Text('Hello World',style: TextStyle(color: Colors.green[100]),)),
+      backgroundColor: Colors.red[140],
     ),
   ));
 }
