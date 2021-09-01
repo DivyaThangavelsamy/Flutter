e018180d80ff1a088af5d80bc073409b3d1b46d5
Divya Thangavelsamy <divthang@WKMZTE892DD6.global.publicisgroupe.net>
2021-09-01 15:20:27 +0100

    flutter container - border and border radius,flutter column widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,38 @@ void main() {
         backgroundColor: Colors.red[900],
         title: Center(child: Text('My first app')),
       ),
-      body: Container(
-        color: Colors.red,
+      /*body: Container(
         child: Text("Hello World"),
         padding: EdgeInsets.all(40),
         margin: EdgeInsets.fromLTRB(20, 30, 20, 30),
+        decoration: BoxDecoration(
+          border: Border.all(width: 2, color:Colors.red),
+          borderRadius: const BorderRadius.all(const Radius.circular(50)),
+          )
+        ),*/
+      body: Column(
+        children: [
+          Container(
+            color: Colors.red,
+            child: Text('First'),
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(20),
+          ),
+          Container(
+            color: Colors.red,
+            child: Text('Second'),
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(20),
+          ),
+          Container(
+            color: Colors.red,
+            child: Text('Third'),
+            padding: EdgeInsets.all(20),
+            margin: EdgeInsets.all(20),
+          )
+        ],
+      ),
       )
     ),
-  ));
+  );
 }
