baf13364b9a64e4eac40be209d4bab1fa329497a
Divya Thangavelsamy <divthang@WKMZTE892DD6.global.publicisgroupe.net>
2021-09-01 15:48:26 +0100

    icon button with tooltip

--- lib/main.dart
+++ lib/main.dart
@@ -7,28 +7,17 @@ void main() {
         backgroundColor: Colors.red[900],
         title: Center(child: Text('My first app')),
       ),
-      /*body: Container(
-        child: Text("Hello World"),
-        padding: EdgeInsets.all(40),
-        margin: EdgeInsets.fromLTRB(20, 30, 20, 30),
-        decoration: BoxDecoration(
-          border: Border.all(width: 2, color:Colors.red),
-          borderRadius: const BorderRadius.all(const Radius.circular(50)),
-          )
-        ),*/
-      body: Container(
-        color: Colors.blue,
-        height: 30,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-           Text('First'),
-            Text('Second'),
-            Text('Third'),
-          ],
-        ),
-      ),
-      )
+          body: IconButton(
+            tooltip: 'Set Alarm',
+            iconSize: 50,
+            icon: Icon(
+              Icons.access_alarm,
+              color: Colors.blue,
+            ), onPressed: () {
+              print("Icon button is pressed");
+          },
+
     ),
-  );
+    ),
+  ));
 }
