054ee59fe9928a52dd4257f9a2846d2fc9f4709b
Divya Thangavelsamy <divthang@WKMZTE892DD6.global.publicisgroupe.net>
2021-09-03 16:17:17 +0100

    quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,60 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('My first app')),
+import './question.dart';
+import './answer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      {
+        'questionText': 'What\'s your fav color',
+        'answerText': ['blue', 'orange', 'red', 'peach'],
+      },
+      {
+        'questionText': 'What\'s your fav animal',
+        'answerText': ['dog', 'cat', 'tiger'],
+      },
+      {
+        'questionText': 'What\'s your fav place',
+        'answerText': ['london', 'india', 'America'],
+      }
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'].toString(),
+            ),
+            ...(questions[_questionIndex]['answerText'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+          ],
+        ),
       ),
-      body: Center(child: Text('Hello World')),
-    ),
-  ));
+    );
+  }
 }
